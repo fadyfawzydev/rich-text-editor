ab99e78b43eeab90b76adcb3109abb55f0230f56
fadyfawzydev <fady.fawzy@theaddressholding.com>
2024-02-05 16:52:28 +0200

    adding some plugins

--- src/app/_components/RichEditor.tsx
+++ src/app/_components/RichEditor.tsx
@@ -1,12 +1,23 @@
 "use client";
-
+import { useState } from "react";
 import dynamic from "next/dynamic";
 import BundledEditor from "./tinyMce/BundledEditor";
 
 const RichEditor = () => {
+  const [content, setContent] = useState<string>(""); // Initial content
+  const handleContentChange = (newContent: string) => {
+    console.log("New content:", newContent);
+    setContent(newContent);
+  };
+
   return (
     <>
-      <BundledEditor />
+      <BundledEditor onContentChange={handleContentChange} />
+      <div>
+        <h2>HTML Content:</h2>
+
+        <div className="">{content}</div>
+      </div>
     </>
   );
 };
--- src/app/_components/tinyMce/BundledEditor.tsx
+++ src/app/_components/tinyMce/BundledEditor.tsx
@@ -50,7 +50,13 @@ import "tinymce/skins/ui/oxide/skin.min.css";
 // import contentUiCss from "!!raw-loader!tinymce/skins/ui/oxide/content.min.css";
 
 export default function BundledEditor(props: any) {
-  const { init, ...rest } = props;
+  const { init, onContentChange, ...rest } = props;
+
+  const handleEditorChange = (content: string) => {
+    if (onContentChange) {
+      onContentChange(content);
+    }
+  };
 
   // note that skin and content_css is disabled to avoid the normal
   // loading process and is instead loaded as a string via content_style
@@ -58,11 +64,14 @@ export default function BundledEditor(props: any) {
     <Editor
       init={{
         ...init,
-        plugins:
-          "ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss",
-        toolbar:
-          "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+        forced_root_block: "", // Ensure no additional root block is added
 
+        plugins:
+          "ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss directionality preview autoresize accordion fullscreen",
+        toolbar:
+          "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight ltr rtl | tinycomments | checklist numlist bullist accordion indent outdent | emoticons charmap | removeformat | preview fullscreen",
+        autoresize_overflow_padding: 50,
+        min_height: 600,
         skin: false,
         content_css: false,
         content_style: [
@@ -71,6 +80,7 @@ export default function BundledEditor(props: any) {
           init?.content_style || "",
         ].join("\n"),
       }}
+      onEditorChange={handleEditorChange}
       {...rest}
     />
   );

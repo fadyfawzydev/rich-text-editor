4f5c102a699efca8a557b706a259d5e62a22d8c2
fadyfawzydev <fady.fawzy@theaddressholding.com>
2024-02-05 15:43:55 +0200

    Styling 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import RichEditor from "./_components/RichEditor";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-xl ">Estate BookTiny MCE</h1>
+    <main className="flex flex-col items-center min-h-screen py-2">
+      <h1 className="text-2xl mb-4">Estate Book Tiny MCE </h1>
       <RichEditor />
     </main>
   );
